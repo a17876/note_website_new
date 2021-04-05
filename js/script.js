adfea7bd9e8e1566929aaebbfc4a885607e13824
Kaylyn <a17876@naver.com>
2021-04-05 15:13:20 -0700

    add function plans with plane text in script.js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//- Button click function
+function button(){
+    using toggle, return true when the button is clicked 
+}
+
+//- Dark Theme
+function darkTheme(){
+    if (dark_theme button function === True){
+        when Button function return true, 
+        change main and sidebar - background color and font color, 
+        change buttons' background color and font color, value to "Light Theme",
+}
+// - Cancel
+function cancel(){
+    if (cancel button function === True){
+        hide textarea, save and cancel button
+    }
+}
+
+// - declare array
+
+let noteArray = {title:"note one", body:"this is my first note"}
+
+// - Save
+function save(){
+    if (save button function === True){
+        alert - ask the title of the note
+        add the title and contents to the note Array
+        add a list item to the sidebar
+    }
+}
+// - list of sidebar
+function display(){
+    if (a list of title in the side bar is clicked){
+        search the title in the Array(using loop)
+        display the title and text contents
+    }
+}
