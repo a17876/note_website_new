9fa41df39474b7f628a2778232eec1a55d2afda2
Kaylyn <a17876@naver.com>
2021-04-12 21:31:46 -0700

    edit js - display text content, fix new not button clear problem

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ const darkButton = document.querySelector(".dark");
 const cancelButton = document.querySelector(".cancel");
 const newButton = document.querySelector(".new");
 const saveButton = document.querySelector(".save");
-const notesArray = [{title:"note one", body:"this is my first note"}];
+const list = document.querySelector(".lst"); 
+  
+var notesArray = [{title:"note one", body:"this is my first note"}];
 
 function changeBG() {
-    document.querySelector(".dark").classList.toggle("dark_mode1");
-    document.querySelector(".cancel").classList.toggle("dark_mode1");
-    document.querySelector(".save").classList.toggle("dark_mode1");
-    document.querySelector(".new").classList.toggle("dark_mode1");
+    darkButton.classList.toggle("dark_mode4");
+    cancelButton.classList.toggle("dark_mode4");
+    saveButton.classList.toggle("dark_mode4");
+    newButton.classList.toggle("dark_mode4");
     document.querySelector(".textarea").classList.toggle("dark_mode1");
     document.querySelector(".aside").classList.toggle("dark_mode1");
     document.querySelector(".body").classList.toggle("dark_mode2");
@@ -23,51 +25,61 @@ function changeBG() {
 
 let textareaVisible = true;
 function hide() {
-    document.querySelector(".cancel").style.visibility = "hidden";
+    cancelButton.style.visibility = "hidden";
     document.querySelector(".textarea").style.visibility = "hidden";
-    document.querySelector(".save").style.visibility = "hidden";
+    saveButton.style.visibility = "hidden";
     textareaVisible = false;
 }
 
 function newNote() {
+    console.log(textareaVisible)   
     if (textareaVisible === false)
     {
-        document.querySelector(".cancel").style.visibility = "visible";
+        cancelButton.style.visibility = "visible";
         document.querySelector(".textarea").style.visibility = "visible";
-        document.querySelector(".save").style.visibility = "visible";
+        saveButton.style.visibility = "visible";
+        textareaVisible = true;
     } else
     {
         document.querySelector(".textarea").value="";
     }
 }
 
-function addList(){
+
+
+function addList()
+{
     userTitle = prompt("Enter the title of your note: ")
     userContent = document.getElementById("content").value;
     notesArray.push({title:`${userTitle}`, body:`${userContent}`})
-
-    const list = document.querySelector(".lst");
     let elem = document.createElement("li");
     let text = document.createTextNode(userTitle);
     elem.appendChild(text);
     list.appendChild(elem);
-    elem.addEventListener("click", diaplayContent);
 }
-
+  
+function diaplayContent(event)
+{
+    noteTitle = event.target.textContent;     
+    console.log(noteTitle)
+    
+    for (let i = 0; i < notesArray.length; ++i){
+        if (notesArray[i].title === noteTitle) {
+            let noteContent = notesArray[i].body;
+            console.log(noteContent)
+            const displayArea = document.getElementById("content");
+            displayArea.value = noteContent
+        }
+    }
+    
+}
 
 darkButton.addEventListener("click", changeBG);
 cancelButton.addEventListener("click", hide);
 newButton.addEventListener("click", newNote);
 saveButton.addEventListener("click", addList);
+list.addEventListener("click", diaplayContent);
 
 
-function diaplayContent(event)
-{
-    noteTitle = event.target.textContent;
-    console.log(noteTitle)
-    for (let i = 0; i < 3; ++i){
-        if (notesArray[i].title.includes(noteTitle)) {
-          console.log(`${notesArray[i].body}`);
-        }
-    }
-}
+
+
